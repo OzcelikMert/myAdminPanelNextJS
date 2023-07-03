bf6470f190caade56b02cb14cb6c254c8e9b4a9a
Mert ÖZÇELİK <mert.ozcelikk@hotmail.com>
2023-07-03 16:14:53 +0300

    things to do

--- src/components/theme/fieldSet/index.tsx
+++ src/components/theme/fieldSet/index.tsx
@@ -13,7 +13,7 @@ class ThemeFieldSet extends Component<PageProps, PageState> {
         return (
             <div className="theme-input static">
                 <span className="label">{this.props.legend} {this.props.legendElement}</span>
-                <div className="field row d-flex m-0">
+                <div className="field row d-flex m-0 pb-3 pt-3">
                     {this.props.children}
                 </div>
             </div>
